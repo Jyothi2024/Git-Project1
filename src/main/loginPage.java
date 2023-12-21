8647d6d2ab636c93cf9d28c9e7230b7cf8ef55b5
JyothiQA <jyothi.qa890@gmail.com>
2023-12-20 23:29:07 -0500

    first commit

---
+++ src/main/loginPage.java
@@ -0,0 +1,10 @@
+package main;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		System.out.println("Lonin page ...");
+
+	}
+
+}
---
+++ src/test/LoninPageTest.java
@@ -0,0 +1,19 @@
+package test;
+
+public class LoninPageTest {
+
+	public static void main(String[] args) {
+	}
+		
+		
+	
+	public static void LPtset() {
+			
+			System.out.println("Lonin page Test ...");
+		}
+		
+		
+
+	}
+
+
